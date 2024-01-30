bedd77f53f4f66e5b6ad532d7d07de7584c18241
Harshit Sai Kannam <kannam.h@northeastern.edu>
2024-01-30 17:00:23 -0500

    in class activities

--- quiz/scripts/closure.js
+++ quiz/scripts/closure.js
@@ -1,9 +1,12 @@
 function setColor(set) {
         let changeColor = set;
-        if(changeColor) {
+        return () => {
+            if(changeColor) {
             let userColor = document.getElementById('color').value;
             document.getElementById('myPara').style.color = userColor;
+
         }
+    }
         
 }
 
--- quiz/scripts/logger.js
+++ quiz/scripts/logger.js
@@ -1,1 +1,16 @@
 // Define a JavaScript function called logMsg() that can be used to log an error message for any object that contains the property errMsg. 
+const logger = {
+    logMsg: function(){
+        if (this.errMsg){
+            console.log(`Error Message from : ${this.errMsg}`);
+        }
+
+    }
+}
+
+const obj1 ={
+    errMsg : "Err in obj 1"
+}
+
+
+logger.logMsg.call(obj1)
